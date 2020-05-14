d590b93214b5db56ba76b5133e70985e34793acc
danielSoler93 <daniel.soler@e-campus.uab.cat>
2020-05-14 17:30:46 +0200

    Add helper script to provide train test dataframes

---
+++ drug_learning/datasets/CYP/provide_dataset.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+TRAIN = os.path.join(DIR, "CYP2C9_dataset_training.csv")
+TEST = os.path.join(DIR, "CYP2C9_dataset_testing.csv")
+DATASET = [TRAIN, TEST]
+
+class CYPDataset:
+
+    def __init__(self):
+        self.train, self.test = DATASET
+
+    def get(self):
+        """
+        Return training and test set for CYPs
+        :return:
+            train-pandas_dataframe
+            test-pandas_dataframe
+        """
+        return  pd.read_csv(self.train), pd.read_csv(self.test)
