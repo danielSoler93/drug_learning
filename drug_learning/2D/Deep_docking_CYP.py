bfea712fa1f3dc57479bd366ecf49e3a55fe605f
cescgina <cescgina@gmail.com>
2020-06-08 20:23:35 +0200

    Linter fixes in Deep_docking_CYP script

--- drug_learning/2D/Deep_docking_CYP.py
+++ drug_learning/2D/Deep_docking_CYP.py
@@ -4,9 +4,7 @@ import os
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import matplotlib.pyplot as plt
 from itertools import product
-from sklearn.metrics import confusion_matrix
 from tensorboard.plugins.hparams import api as hp
 from sklearn.model_selection import train_test_split
 
@@ -15,29 +13,31 @@ PATH_DATA = "../datasets/CYP/"
 
 def construct_optimizer(hparams):
     if hparams[HP_OPTIMIZER] == "adam":
-        return tf.keras.optimizers.Adam(learning_rate = hparams[HP_LR])
+        return tf.keras.optimizers.Adam(learning_rate=hparams[HP_LR])
     elif hparams[HP_OPTIMIZER] == "sgd":
-        return tf.keras.optimizers.SGD(learning_rate = hparams[HP_LR])
+        return tf.keras.optimizers.SGD(learning_rate=hparams[HP_LR])
     elif hparams[HP_OPTIMIZER] == "RMSprop":
-        return tf.keras.optimizers.RMSprop(learning_rate = hparams[HP_LR])
+        return tf.keras.optimizers.RMSprop(learning_rate=hparams[HP_LR])
+
 
 def train_test_model(hparams):
     internal_layers = [tf.keras.layers.Dropout(hparams[HP_DROPOUT])]+[tf.keras.layers.Dense(hparams[HP_NEURONS], kernel_regularizer=tf.keras.regularizers.l2(0.001), activation='relu') for _ in range(hparams[HP_HIDDEN_LAYERS])]
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(1024, activation='relu', input_shape=(1024,))]+
+        tf.keras.layers.Dense(1024, activation='relu', input_shape=(1024,))] +
         internal_layers+[tf.keras.layers.Dense(1, activation="sigmoid")]
     )
-    model.compile(optimizer=construct_optimizer(hparams), loss="binary_crossentropy", metrics=[tf.keras.metrics.BinaryAccuracy(name="binary_accuracy")])
+    model.compile(optimizer=construct_optimizer(hparams), loss="binary_crossentropy",
+                  metrics=[tf.keras.metrics.BinaryAccuracy(name="binary_accuracy")])
     model.fit(train_data, train_labels, epochs=5)
     _, results = model.evaluate(test_data, test_labels)
     return results
 
 
 def run(run_dir, hparams):
-  with tf.summary.create_file_writer(run_dir).as_default():
-    hp.hparams(hparams)  # record the values used in this trial
-    accuracy = train_test_model(hparams)
-    tf.summary.scalar("accuracy", accuracy, step=1)
+    with tf.summary.create_file_writer(run_dir).as_default():
+        hp.hparams(hparams)  # record the values used in this trial
+        accuracy = train_test_model(hparams)
+        tf.summary.scalar("accuracy", accuracy, step=1)
 
 
 shared_data = pd.read_csv(os.path.join(PATH_DATA, "shared_set_cyp.csv"))
@@ -55,14 +55,14 @@ features_only_3a4 = np.load("only_3a4_set_features.npy")
 train_data, test_data, train_labels, test_labels = train_test_split(features_shared, labels_2c9, stratify=labels_2c9)
 
 HP_HIDDEN_LAYERS = hp.HParam("hidden_layers", hp.Discrete(list(range(3, 10))))
-HP_NEURONS = hp.HParam("neurons", hp.Discrete([2**i for i in range(7,10)]))
+HP_NEURONS = hp.HParam("neurons", hp.Discrete([2**i for i in range(7, 10)]))
 HP_DROPOUT = hp.HParam("dropout", hp.Discrete([0.2]))
 HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['sgd']))
 HP_L2 = hp.HParam('l2 regularizer', hp.Discrete([.001]))
 HP_LR = hp.HParam("learning_rate", hp.Discrete([0.001, 0.01, 0.1, 1.0, 10.0]))
 
 with tf.summary.create_file_writer('logs_2/hparam_tuning').as_default():
-    hp.hparams_config(hparams=[HP_HIDDEN_LAYERS,HP_NEURONS, HP_DROPOUT, HP_OPTIMIZER, HP_L2, HP_LR],
+    hp.hparams_config(hparams=[HP_HIDDEN_LAYERS, HP_NEURONS, HP_DROPOUT, HP_OPTIMIZER, HP_L2, HP_LR],
                       metrics=[hp.Metric("accuracy", display_name='Accuracy')])
 
 session_num = 0
