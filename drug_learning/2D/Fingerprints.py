e88cc2ba06b61b73658ef77bf9426598f43b08cc
anasf97 <anasanchezfernandez1997@gmail.com>
2020-07-13 09:24:13 +0200

    Added MACCS and RDkit fps

--- drug_learning/2D/Fingerprints.py
+++ drug_learning/2D/Fingerprints.py
@@ -1,30 +1,91 @@
 import os
 import numpy as np
+import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import AllChem
+from rdkit.Chem import MACCSkeys
 from rdkit.Chem import DataStructs
 
-class MorganFP():
+class NotFittedException(Exception):
+    pass
+
+class Fingerprint():
     def __init__(self):
+        self.filename = None
         self.structures = None
         self.features = None
+        self.fitted = False
 
     def fit(self, input_sdf):
-        if self.structures is None:
-            self.structures = Chem.SDMolSupplier(input_sdf)
+        (self.filename, ext) = os.path.splitext(input_sdf)
+        self.structures = Chem.SDMolSupplier(input_sdf)
+        self.fitted = True
         return self.structures
 
     def transform(self, to_csv = False):
+        if not self.fitted:
+            raise NotFittedException("Must fit the model before transform")
+
+
+class MorganFP(Fingerprint):
+
+    def transform(self, to_csv = False):
+        super().transform(to_csv = False)
         fts = []
-        fps = []
-        if self.features is None:
-            for mol in self.structures:
-                fp = AllChem.GetMorganFingerprintAsBitVect(mol,2,nBits=1024)
-                fps.append(fp)
-                arr = np.zeros((0,), dtype=np.int8)
-                DataStructs.ConvertToNumpyArray(fp,arr)
-                fts.append(arr)
-                self.features = np.array(fts)
+        mol_names = []
+        for mol in self.structures:
+            fp = AllChem.GetMorganFingerprintAsBitVect(mol,2,nBits=1024)
+            arr = np.zeros((0,), dtype=np.int8)
+            DataStructs.ConvertToNumpyArray(fp,arr)
+            fts.append(arr)
+            self.features = np.array(fts)
+            mol_names.append(mol.GetProp("_Name"))
+
+        if to_csv:
+            df = pd.DataFrame(self.features, index = mol_names)
+            df.to_csv(self.filename + "_MorganFP.csv")
+
+        return self.features
+
+class MACCS_FP(Fingerprint):
+
+    def transform(self, to_csv = False):
+        super().transform(to_csv = False)
+        fts = []
+        mol_names = []
+        for mol in self.structures:
+            fp = MACCSkeys.GenMACCSKeys(mol)
+            arr = np.zeros((0,), dtype=np.int8)
+            DataStructs.ConvertToNumpyArray(fp,arr)
+            fts.append(arr)
+            self.features = np.array(fts)
+            mol_names.append(mol.GetProp("_Name"))
+
+        if to_csv:
+            df = pd.DataFrame(self.features, index = mol_names)
+            df.to_csv(self.filename + "_MACCS_FP.csv")
+
+        return self.features
+
+
+class RDkitFP(Fingerprint):
+
+    def transform(self, to_csv = False):
+        super().transform(to_csv = False)
+        fts = []
+        mol_names = []
+        for mol in self.structures:
+            fp = Chem.RDKFingerprint(mol)
+            arr = np.zeros((0,), dtype=np.int8)
+            DataStructs.ConvertToNumpyArray(fp,arr)
+            fts.append(arr)
+            self.features = np.array(fts)
+            mol_names.append(mol.GetProp("_Name"))
+
+        if to_csv:
+            df = pd.DataFrame(self.features, index = mol_names)
+            df.to_csv(self.filename + "_RDkitFP.csv")
+
         return self.features
 
 
@@ -33,5 +94,5 @@ if __name__ == "__main__":
     morgan_fps = MorganFP()
     morgan_fps.fit("../datasets/ligands.sdf")
     morgan_fps.structures
-    morgan_fps.transform()
+    morgan_fps.transform(to_csv = True)
     morgan_fps.features
